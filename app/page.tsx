7d3ab5c8b1aa4d64710b0bcddb7ac00b1205678d
Josephine Josephine <josephinedjojo@gmail.com>
2024-01-06 19:49:37 +0700

    fix: adjust to no-cache

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ const getRooms = async (searchParams: string) => {
   const urlParams = new URLSearchParams(searchParams);
   const queryString = urlParams.toString();
   const res = await fetch(`${process.env.API_URL}/api/rooms?${queryString}`, {
-    cache: "no-store",
+    cache: "no-cache",
   });
   return res.json();
 };
